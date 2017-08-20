bbfd68e7b737df77735bb55ca4bd7fbf9222a052
Haze <admin@haze.pw>
2017-08-19 23:39:29 -0400

    my head hurts but we have a start to opcodes

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,5 +1,3 @@
-
-
 pub mod ill {
     use std::fs::File;
     use std::io::Read;
@@ -10,12 +8,14 @@ pub mod ill {
     use std::fmt::{Display, Formatter};
 
     use opcodes::ill::OpCode;
+    use opcodes::ill::ExpressionType;
+    use opcodes::ill::{r_container, r_literal, r_register, r_variable};
 
     use NamedFile;
     use IllError::*;
 
     const NEWLINE: char = '\n';
-    const STACK_DEF: char = '+';
+    const Register_DEF: char = '+';
     const DEF_END: char = ';';
 
 
@@ -28,14 +28,14 @@ pub mod ill {
     const INST_CODES_BEGIN: char = '{';
     const INST_CODES_END: char = '}';
 
-
+    // commands
+    const COMMENT_SINGLE_LINE: char = '|';
 
     #[derive(Default, Debug)]
-    pub struct Stack {
+    pub struct Register {
         pub identifier: String,
         pub value: usize,
-        pub is_variable: bool,
-    }
+        pub is_variable: bool,    }
 
     #[derive(Debug)]
     pub struct EnhancedFile {
@@ -62,25 +62,29 @@ pub mod ill {
 
     #[derive(Debug)]
     pub enum IllError {
-        StackRefinition(ReadHead, String),
-        NoStacksFound(EnhancedFile),
+        RegisterRefinition(ReadHead, String),
+        NoregistersFound(EnhancedFile),
         UnexpectedCharacter(ReadHead, char, Option<String>),
         InstructionRedefinition(ReadHead, String),
         UnknownOpCode(ReadHead, String),
         InvalidOpCodeArguments(ReadHead, String),
         OpCodeArgumentMismatch(ReadHead, String, i32, i32),
+        NoMainInstruction(),
+        OpCodeInvalidArgument(ReadHead, ExpressionType, String), // wanted, got
     }
 
     impl Error for IllError {
         fn description(&self) -> &str {
             match *self {
-                StackRefinition(_, _) => "A stack redefinition was attempted.",
-                NoStacksFound(_) => "No stack definitions found.",
+                RegisterRefinition(_, _) => "A Register redefinition was attempted.",
+                NoregistersFound(_) => "No Register definitions found.",
                 UnexpectedCharacter(_, _, _) => "Unexpected character found.",
                 InstructionRedefinition(_, _) => "A instruction redefinition was attempted.",
                 UnknownOpCode(_, _) => "An unknown OpCode was used.",
                 InvalidOpCodeArguments(_, _) => "An invalid instruction for an OpCode was found.",
                 OpCodeArgumentMismatch(_, _, _, _) => "Opcode has too few or many arguments.",
+                NoMainInstruction() => "No Main Instruction was found",
+                OpCodeInvalidArgument(_, _, _) => "Argument mismatch in OpCode"
             }
         }
     }
@@ -88,13 +92,15 @@ pub mod ill {
     impl IllError {
         pub fn name(&self) -> String {
             String::from(match *self {
-                StackRefinition(_, _) => "Stack Redefinition",
-                NoStacksFound(_) => "No Stack Found",
+                RegisterRefinition(_, _) => "Register Redefinition",
+                NoregistersFound(_) => "No Register Found",
                 UnexpectedCharacter(_, _, _) => "Unexpected Character",
                 InstructionRedefinition(_, _) => "Instruction Redefinition",
                 UnknownOpCode(_, _) => "Unknown OpCode",
                 InvalidOpCodeArguments(_, _) => "Invalid OpCode Instruction",
                 OpCodeArgumentMismatch(_, _, _, _) => "OpCode Argument Length Mismatch",
+                NoMainInstruction() => "No Main Instruction",
+                OpCodeInvalidArgument(_, _, _) => "Argument Mismatch"
             })
         }
     }
@@ -105,10 +111,10 @@ pub mod ill {
                 format!("[{}:{}]", rh.line, rh.column)
             }
             match *self {
-                StackRefinition(ref rh, ref name) => {
+                RegisterRefinition(ref rh, ref name) => {
                     write!(
                         f,
-                        "Err@{} => The stack named \"{}\" already exists!",
+                        "Err@{} => The Register named \"{}\" already exists!",
                         fmt_rh(rh),
                         name
                     )
@@ -121,10 +127,10 @@ pub mod ill {
                         name
                     )
                 }
-                NoStacksFound(ref e_file) => {
+                NoregistersFound(ref e_file) => {
                     write!(
                         f,
-                        "Cannot find a stack definition for {:?}.",
+                        "Cannot find a Register definition for {:?}.",
                         e_file.filename
                     )
                 }
@@ -163,6 +169,13 @@ pub mod ill {
                         given
                     )
                 }
+                NoMainInstruction() => {
+                    write!(f, "No Main instruction was found for any input files.")
+                }
+                OpCodeInvalidArgument(ref rh, ref e_type, ref got) => {
+                    write!(f, "Err@{} => Expected a {}, but got \"{}\" instead.", fmt_rh(rh), 
+                    e_type.name(), got)
+                }
             }
         }
     }
@@ -198,7 +211,7 @@ pub mod ill {
     struct Instruction {
         name: String,
         codes: Vec<OpCode>,
-        scope: Vec<Stack>,
+        scope: Vec<Register>,
         arguments: Vec<String>,
         is_main: bool,
     }
@@ -208,7 +221,7 @@ pub mod ill {
         debug: bool,
         files: Vec<EnhancedFile>,
         opcodes: Vec<OpCode>, // valid opcodes
-        stacks: Vec<Stack>,
+        registers: Vec<Register>,
         instructions: Vec<Instruction>,
     }
 
@@ -266,7 +279,7 @@ pub mod ill {
         fn find_opcode(&self, name: String) -> Option<&OpCode> {
             self.opcodes.iter().find(|x: &&OpCode| x.name == name)
         }
-
+        
         fn does_opcode_exist(&self, name: String) -> bool {
             self.find_opcode(name).is_some()
         }
@@ -302,12 +315,12 @@ pub mod ill {
             }
         }
 
-        fn find_stack(&self, name: String) -> Option<&Stack> {
-            self.stacks.iter().find(|x: &&Stack| x.identifier == name)
+        fn find_Register(&self, name: String) -> Option<&Register> {
+            self.registers.iter().find(|x: &&Register| x.identifier == name)
         }
 
-        fn does_stack_exist(&self, name: String) -> bool {
-            self.find_stack(name).is_some()
+        fn does_register_exist(&mut self, name: String) -> bool {
+            self.find_Register(name).is_some()
         }
 
         fn find_instruction(&self, name: String) -> Option<&Instruction> {
@@ -320,25 +333,69 @@ pub mod ill {
             self.find_instruction(name).is_some()
         }
 
-        fn parse_code(&self, rh: ReadHead, code: String) -> Result<OpCode, IllError> {
+        fn parse_code(&mut self, rh: ReadHead, code: String) -> Result<OpCode, IllError> {
             let data: Vec<String> = code.split(' ').map(String::from).collect::<Vec<String>>();
             let code_name = data[0].clone();
             let nls = newlines(&code) as usize;
+            let error_rh = rh.new_by(-(nls as i32), ((-rh.column) + code.len() as i32));
             if !self.does_opcode_exist(code_name.clone()) {
                 return Err(IllError::UnknownOpCode(
-                    rh.new_by(-(nls as i32), ((-rh.column) + code.len() as i32)),
+                    error_rh,
                     data[0].clone(),
                 ));
             }
             let opcode = self.find_opcode(code_name.clone()).unwrap();
             if (data.len() - 1) != opcode.arguments.len() {
                 return Err(IllError::OpCodeArgumentMismatch(
-                    rh.new_by(-(nls as i32), ((-rh.column) + code.len() as i32)),
+                    error_rh,
                     data[0].clone(),
                     opcode.arguments.len() as i32,
                     (data.len() - 1) as i32,
                 ));
             }
+
+            fn is_arg_literal(arg: String) -> bool {
+                arg.parse::<usize>().is_ok()
+            }
+
+            let name = opcode.name.clone();
+            let mut args = opcode.arguments.clone();
+            
+            for i in 0 .. args.len() {
+                let expected = args[i].clone();
+                let ref argument = data[i + 1];
+                println!("arg = {}, expected = {:?}", argument, expected);
+                match expected {
+                    ExpressionType::IntegerLiteral(_) => {
+                        if !is_arg_literal(argument.clone()) {
+                            return Err(OpCodeInvalidArgument(
+                                error_rh,
+                                ExpressionType::IntegerLiteral(0),
+                                argument.clone()
+                            ));
+                        } else { // See if I can do this by refrence? (looks to be too complicated, I don't want to have to use refcells)
+                            let new_opcode = 
+                            args[i] = ExpressionType::IntegerLiteral(argument.parse::<usize>().unwrap());
+                        }
+                    }
+                    ExpressionType::ContainerRefrence(_) => {
+                        if !self.does_register_exist(argument.clone()) {
+                            
+                        } else {
+                            args[i] = ExpressionType::ContainerRefrence(argument.clone());
+                        }
+                    }
+                    ExpressionType::RegisterRefrence(_) => {
+
+                    }
+
+                    ExpressionType::VariableRefrence(_) => {
+
+                    }
+                }
+            }
+
+                
             Ok(OpCode::new_str(code_name))
         }
 
@@ -348,23 +405,29 @@ pub mod ill {
                 read_until(it, vec![INST_PARAM_BEGIN])
             }
 
-            for e_file in &self.files {
+            for e_file in self.files {
                 let mut it = e_file.content.chars().peekable();
                 let mut head: ReadHead = ReadHead::new();
                 let mut cur_inst: Instruction = Default::default();
                 let mut cur_inst_sb: InstSwitchBox = Default::default();
                 while let Some(x) = it.next() {
                     head.advance(x);
-                    if x == INST_DEF {
+                    if x == COMMENT_SINGLE_LINE {
+                        dump_until(&mut head, it.by_ref(), vec![NEWLINE]);
+                    } else if x == INST_DEF {
                         if cur_inst_sb.is_reading_definition {
-                            return Err(UnexpectedCharacter(head, x, Some(String::from(" expecting instruction identifier."))));
+                            return Err(UnexpectedCharacter(
+                                head,
+                                x,
+                                Some(String::from(" expecting instruction identifier.")),
+                            ));
                         } else {
                             cur_inst_sb.is_reading_definition = true;
                         }
                         if cur_inst_sb.is_reading_definition {
                             cur_inst.is_main = *it.peek().unwrap() == INST_DEF;
-                            let stack_name = traverse_read(&mut head, read_inst_def(it.by_ref()));
-                            cur_inst.name = stack_name;
+                            let Register_name = traverse_read(&mut head, read_inst_def(it.by_ref()));
+                            cur_inst.name = Register_name;
                             cur_inst_sb.is_reading_arguments = true;
                             let params_unsp =
                                 traverse_read(
@@ -377,15 +440,34 @@ pub mod ill {
                                 .collect();
                             cur_inst.arguments = params;
                             cur_inst_sb.is_reading_arguments = false;
+                            if !any_exists_until(
+                                &mut it.clone(),
+                                vec![INST_CODES_BEGIN],
+                                vec![INST_CODES_END],
+                            )
+                            {
+                                return Err(UnexpectedCharacter(
+                                    head,
+                                    x,
+                                    Some(format!(
+                                        " expecting instruction code beginning \"{}\".",
+                                        INST_CODES_BEGIN
+                                    )),
+                                ));
+                            }
                             dump_until(&mut head, it.by_ref(), vec![INST_CODES_BEGIN]);
-                            while *it.peek().unwrap() != INST_CODES_END {
+                            while it.peek().is_some() && *it.peek().unwrap() != INST_CODES_END {
                                 if !any_exists_until(
                                     &mut it.clone(),
                                     vec![DEF_END],
                                     vec![INST_CODES_END],
                                 )
                                 {
+                                    // break because no codes
                                     break;
+                                }
+                                if *it.peek().unwrap() == COMMENT_SINGLE_LINE {
+                                    dump_until(&mut head, it.by_ref(), vec![NEWLINE]);
                                 }
                                 let raw_code = traverse_read(
                                     &mut head,
@@ -413,35 +495,37 @@ pub mod ill {
                     }
                 }
             }
-            if self.instructions.len() == 1 {
+            if self.instructions.len() == 0 {
+                return Err(NoMainInstruction());
+            } else if self.instructions.len() == 1 {
                 self.instructions[0].is_main = true;
             }
             println!("insts = {:?}", self.instructions);
             Ok(())
         }
 
-        fn create_stacks(&mut self) -> Result<(), IllError> {
+        fn create_registers(&mut self) -> Result<(), IllError> {
 
             for e_file in &self.files {
                 let mut iter = e_file.content.chars().peekable();
                 let mut head: ReadHead = ReadHead::new();
-                let mut has_found_stacks: bool = false;
+                let mut has_found_registers: bool = false;
                 while let Some(x) = iter.next() {
                     head.advance(x);
                     if !x.is_whitespace() {
-                        if x == STACK_DEF {
-                            has_found_stacks = true;
+                        if x == Register_DEF {
+                            has_found_registers = true;
                             while iter.peek().is_some() && *iter.peek().unwrap() != NEWLINE {
-                                let stack_name = traverse_read(
+                                let Register_name = traverse_read(
                                     &mut head,
-                                    read_until(iter.by_ref(), vec![DEF_END, NEWLINE]),
+                                    read_until(iter.by_ref(), vec![DEF_END]),
                                 );
-                                if self.does_stack_exist(stack_name.clone()) {
-                                    let err_str = stack_name.clone();
-                                    return Err(StackRefinition(head, err_str));
+                                if self.does_register_exist(Register_name.clone()) {
+                                    let err_str = Register_name.clone();
+                                    return Err(RegisterRefinition(head, err_str));
                                 }
-                                self.stacks.push(Stack {
-                                    identifier: stack_name,
+                                self.registers.push(Register {
+                                    identifier: Register_name,
                                     is_variable: false,
                                     ..Default::default()
                                 });
@@ -450,17 +534,17 @@ pub mod ill {
                         }
                     }
                 }
-                if !has_found_stacks {
-                    return Err(NoStacksFound(e_file.clone()));
+                if !has_found_registers {
+                    return Err(NoregistersFound(e_file.clone()));
                 } else if self.debug {
-                    println!("Found stacks: {:?}", self.stacks);
+                    println!("Found registers: {:?}", self.registers);
                 }
             }
             Ok(())
         }
 
         pub fn begin_parsing(&mut self) -> Result<(), IllError> {
-            let res = self.create_stacks();
+            let res = self.create_registers();
             if res.is_err() {
                 return res;
             }
--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,40 +1,73 @@
 
 pub mod ill {
-    use interpreter::ill::Stack;
+    use interpreter::ill::Register;
+    use opcodes::ill::ExpressionType::*;
+
+    #[derive(Debug, Clone)]
+    pub enum ExpressionType {
+        IntegerLiteral(usize),
+        ContainerRefrence(String), // both stacks and variables, no difference
+        RegisterRefrence(String), // Stack Name
+        VariableRefrence(String) // Variable Name
+    }
 
 
-
-    // why did i write shit lol
-    fn stack_helper(var: bool) -> Stack {
-        Stack {
-            is_variable: var,
-            ..Default::default()
+    impl ExpressionType {
+        pub fn name(&self) -> String {
+            String::from(match *self {
+                IntegerLiteral(_) => "Integer Literal",
+                ContainerRefrence(_) => "Container Refrence",
+                RegisterRefrence(_) => "Register Refrence",
+                VariableRefrence(_) => "Variable Refrence"
+            })
         }
     }
 
-    fn register() -> Stack {
-        stack_helper(false)
+    pub fn literal() -> ExpressionType {
+        ExpressionType::IntegerLiteral(0 as usize)
     }
 
-    fn variable() -> Stack {
-        stack_helper(true)
+    pub fn container() -> ExpressionType {
+        ExpressionType::ContainerRefrence(String::new())
     }
 
+    pub fn register() -> ExpressionType {
+        ExpressionType::RegisterRefrence(String::new())
+    }
+
+    pub fn variable() -> ExpressionType {
+        ExpressionType::VariableRefrence(String::new())
+    }
+
+    pub fn r_literal(it: usize) -> ExpressionType { ExpressionType::IntegerLiteral(it) }
+    pub fn r_container(it: String) -> ExpressionType { ExpressionType::ContainerRefrence(it) }
+    pub fn r_register(it: String) -> ExpressionType { ExpressionType::RegisterRefrence(it) }
+    pub fn r_variable(it: String) -> ExpressionType { ExpressionType::VariableRefrence(it) }
+
+
+    pub fn do_opcode(code: OpCode) {
+        // placeholder
+    }
 
     // i've always wanted a modular language...
     pub fn default_opcodes() -> Vec<OpCode> {
         let mut opcodes: Vec<OpCode> = Vec::new();
-        opcodes.push(OpCode::new("mov").expecting(register()).expecting(variable()));
+        opcodes.push(OpCode::new("mov").expecting(container()).expecting(literal()));
+        opcodes.push(OpCode::new("mak").expecting(variable()).expecting(literal()));
+        opcodes.push(OpCode::new("cop").expecting(container()).expecting(container()));
         opcodes
     }
 
-    #[derive(Default, Debug)]
+    #[derive(Default, Debug, Clone)]
     pub struct OpCode {
         pub name: String,
-        pub arguments: Vec<Stack>,
+        pub arguments: Vec<ExpressionType>,
     }
 
     impl OpCode {
+        pub fn update(&mut self, index: usize, e_type: ExpressionType) {
+            self.arguments[index] = e_type;
+        }
         pub fn new_str(name: String) -> OpCode {
             OpCode {
                 name: name,
@@ -46,7 +79,9 @@ pub mod ill {
             OpCode::new_str(String::from(name))
         }
 
-        pub fn expecting(self, some: Stack) -> OpCode {
+
+        // also named 'with'
+        pub fn expecting(self, some: ExpressionType) -> OpCode {
             let mut args = self.arguments;
             args.push(some);
             OpCode {
